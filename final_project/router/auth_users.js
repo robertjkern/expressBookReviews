813f33f13b4393184a3fbee061d573820e47d1c9
robertjkern <robert.kern@amway.com>
2026-02-11 12:11:28 -0500

    Cleaning up code.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,6 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
 let validusers = users.filter((user) => {
         return (user.username === username && user.password === password);
     });
